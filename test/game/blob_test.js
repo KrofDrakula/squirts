9459c909ecf4359d870cfd4773de30182e6fd1d9
Klemen Slavič <krof.drakula@gmail.com>
2012-09-18 17:37:00 +0200

    Fixed the mass calculation bug

---
+++ test/game/blob_test.js
@@ -0,0 +1,32 @@
+describe('Blob', function() {
+    describe('Calculated properties', function() {
+        it('#mass', function() {
+            var b = new Blob({
+                position : new Vector2d(0, 0),
+                speed    : new Vector2d(0, 0),
+                radius   : 10
+            });
+
+            b.mass.should.equal(100 * Math.PI);
+
+            b.mass += 100;
+
+            b.mass.should.be.closeTo(100 * Math.PI + 100, 0.01);
+        });
+
+        it('#momentum', function() {
+            var b = new Blob({
+                position : new Vector2d(0, 0),
+                speed    : new Vector2d(0, 0),
+                radius   : 10
+            });
+
+            b.momentum.should.be.deep.equal(new Vector2d(0, 0));
+
+            b.speed = b.speed.add(1);
+
+            b.momentum.x.should.be.closeTo(Math.PI * 100, 0.01);
+            b.momentum.y.should.be.closeTo(Math.PI * 100, 0.01);
+        });
+    });
+});
