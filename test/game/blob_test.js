ec0f4e840966ed90ca78824f4e10bcb4c73a3ff7
Klemen Slavič <krof.drakula@gmail.com>
2012-09-18 17:48:56 +0200

    Adding tests

--- test/game/blob_test.js
+++ test/game/blob_test.js
@@ -28,5 +28,25 @@ describe('Blob', function() {
             b.momentum.x.should.be.closeTo(Math.PI * 100, 0.01);
             b.momentum.y.should.be.closeTo(Math.PI * 100, 0.01);
         });
+
+        it('#intersectsWith(blob)', function() {
+            var a = new Blob({
+                position : new Vector2d(0, 0),
+                speed    : new Vector2d(0, 0),
+                radius   : 4
+            });
+
+            var b = new Blob({
+                position : new Vector2d(5, 0),
+                speed    : new Vector2d(0, 0),
+                radius   : 1.1
+            });
+
+            a.intersectsWith(b).should.be.true;
+            b.position.x -= 2;
+            a.intersectsWith(b).should.be.true;
+            b.position.x += 4;
+            a.intersectsWith(b).should.be.false;
+        });
     });
 });
