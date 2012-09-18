6a2d2f26f8af9022dd2e3f043b26f433c13188b3
Klemen Slavič <krof.drakula@gmail.com>
2012-09-18 21:51:48 +0200

    Added minification step
    Also added init code to the concat step to keep it in a single file.

--- grunt.js
+++ grunt.js
@@ -2,15 +2,21 @@ module.exports = function(grunt) {
     grunt.initConfig({
         concat: {
             dist: {
-                src  : ['lib/common.js', 'lib/math/*.js', 'lib/game/*.js'],
+                src  : ['lib/common.js', 'lib/math/*.js', 'lib/game/*.js', 'squirts.js'],
                 dest : 'build/game.js'
             }
         },
         watch: {
             files : '<config:concat.dist.src>',
             tasks : 'default'
+        },
+        min: {
+            dist: {
+                src: ['build/game.js'],
+                dest: 'build/game.min.js'
+            }
         }
     });
 
-    grunt.registerTask('default', 'concat');
+    grunt.registerTask('default', 'concat min');
 };
